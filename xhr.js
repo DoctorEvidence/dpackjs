72c6f8041bd46019a3d71b021d0dec4136c3d59a
Kris Zyp <kriszyp@gmail.com>
2018-06-26 15:10:58 -0600

    Enable back-referencing style

--- xhr.js
+++ xhr.js
@@ -2,17 +2,19 @@ var DecodeBuffer = require('msgpack-lite/lib/decode-buffer').DecodeBuffer
 
 exports.DecodedXMLHttpRequest = function() {
     var xhr = new XMLHttpRequest()
-    xhr.responseType = 'arraybuffer'
     var whenProgressDecoded
     var decoder
     xhr.addEventListener('progress', function(event) {
     	var arrayBuffer = xhr.response
     	if (!decoder) {
-    		if (/msgpack/.test(xhr.getResponseHeader('Content-Type')))
+    		if (arrayBuffer && /msgpack/.test(xhr.getResponseHeader('Content-Type'))) {
 	    		decoder = new DecodeBuffer()
+		    	decoder.offset = 0
+    		}
 	    	else
 	    		return
     	}
+    	decoder.buffer = new Uint8Array(arrayBuffer)
     	if (decoder.onResume) {
     		decoder.onResume(arrayBuffer)
     		whenProgressDecoded.then(function(value) {
@@ -20,7 +22,7 @@ exports.DecodedXMLHttpRequest = function() {
     		}, onError)
     	}
 		try {
-			xhr.responseDecoded = decoder.codec.decode(arrayBuffer)
+			xhr.responseDecoded = decoder.codec.decode(decoder)
 		} catch (error) {
 			onError(error)
 		}
