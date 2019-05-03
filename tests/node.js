751a7431f60d07af7894d6fd75b43b06f57eee33
Kris Zyp <kriszyp@gmail.com>
2019-05-02 22:53:54 -0600

    Working towards shared structure for blocks

--- tests/node.js
+++ tests/node.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai')
-const { serialize, parse, parseLazy, createParseStream, createSerializeStream, asBlock, copy, Options, getLazyHeader } = require('..')
+const { serialize, parse, parseLazy, createParseStream, createSerializeStream, createSharedStructure, asBlock, copy, Options, getLazyHeader } = require('..')
 const fs = require('fs')
 var inspector = require('inspector')
 inspector.open(9329, null, true)
@@ -207,24 +207,16 @@ suite('dpack node tests', () => {
 			setTimeout(sendNext)
 		})
 	})
-	test.only('shared structure with blocks', function() {
+	test('shared structure with blocks', function() {
 		var testData = [{ i: 1, block: { a: 1, name: 'one', type: 'odd', isOdd: true }},
 			{ i: 2, block: { a: 2, name: 'two', type: 'even'}},
 			{ i: 3, block: { a: 2.5, name: 'two point five', type: 'decimal'}}
 		]
-		var sharedSerialized, sharedSerializedChild
-		var sharedStructure = createSharedStructure(function(serialize) {
-			var serialized = serialize()
-			sharedSerialized = serialized
-		})
-		var sharedChild = createSharedStructure(function(serialize) {
-			var serialized = serialize()
-			sharedSerializedChild = serialized
-		})
+		var sharedStructure = createSharedStructure()
+		var sharedChild = createSharedStructure()
 		for (var i = 0; i < 3; i++) {
 			testData[i].block = asBlock(testData[i].block, sharedChild)
 		}
-		debugger
 		var serializedWithShared = serialize(testData[0], { shared: sharedStructure })
 		var serializedWithShared1 = serialize(testData[1], { shared: sharedStructure })
 		var serializedWithShared2 = serialize(testData[2], { shared: sharedStructure })
@@ -234,12 +226,12 @@ suite('dpack node tests', () => {
 		assert.deepEqual(parsed, testData[1])
 		var parsed = parse(serializedWithShared2, { shared: sharedStructure })
 		assert.deepEqual(parsed, testData[2])
+		var sharedSerialized = sharedStructure.serialized
+		var sharedSerializedChild = sharedChild.serialized
 		
 		// reset
-		sharedStructure = createSharedStructure(function(serialize) {
-			var serialized = serialize()
-			console.log('shared structure updated', serialized)
-			sharedSerialized = serialized
+		sharedStructure = createSharedStructure(function() {
+			console.log('shared structure updated', sharedStructure.serialized)
 		})
 		parse(sharedSerialized, { shared: sharedStructure })
 		var parsed = parse(serializedWithShared, { shared: sharedStructure })
